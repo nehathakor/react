12a5ac3e9332ff735705edac554b3ff0a940142d
Neha Thakor <neha.k.thakor@gmail.com>
2024-01-12 21:24:27 +0530

    second commit

--- newproject2/src/App.jsx
+++ newproject2/src/App.jsx
@@ -1,8 +1,9 @@
-
+import Card from './components/Card'
 function App() {
   return (
-    <>
-  <h1 className="text-center bg-green-600 text-black rounded p-10 m-10">Tailwind css</h1>
+  <>
+  <h1 className="text-center bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 text-black rounded p-10 m-10">Tailwind css</h1>
+  <Card username="Johnny" btntext="Follow me"/>
   </>
   )
 }
